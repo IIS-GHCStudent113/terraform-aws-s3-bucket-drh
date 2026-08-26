2cb617298587bf477bab7487a43ea07f617370f0
TEK Student <TEKstudent@vlab.lan>
2026-08-26 11:30:14 -0700

    add s3 bucket module from private registry

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 } 
+
+module "s3-bucket-drh" {
+  source  = "app.terraform.io/policy-as-code-training/s3-bucket-drh/aws"
+  version = "1.1.1"
+  # insert required variables here
+  bucket-name = "s3-bucket-drh"
+}
